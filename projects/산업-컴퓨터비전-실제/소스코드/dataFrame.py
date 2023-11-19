f20d16c9825fe4d1e41aead4f09224043d233940
hyunki93 <105183769+hyunki93@users.noreply.github.com>
2023-11-19 22:15:36 +0900

    k-mean-클러스터링 알고리즘
    k-mean-클러스터링 알고리즘

---
+++ projects/산업-컴퓨터비전-실제/소스코드/dataFrame.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.DataFrame(columns=['x', 'y'])
+
+df.loc[0] = [0, 0]
+df.loc[1] = [3, 6]
+df.loc[2] = [2, 2]
+df.loc[3] = [1, 1]
+df.loc[4] = [5, 3]
+df.loc[5] = [4, 6]
+df.loc[6] = [6, 3]
+df.loc[7] = [5, 4]
+df.loc[8] = [6, 4]
+df.loc[9] = [7, 5]
